afa40ac9146c687edf2b645269f9d36cbc54fa3d
Schorzz <Uh2stos!>
2016-10-20 10:34:40 +0200

    ghg

--- src/MainGKA.java
+++ src/MainGKA.java
@@ -1,14 +1,17 @@
-package gka1v2;
-
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Rectangle;
-import java.awt.TextField;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.geom.Rectangle2D;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
 
 import org.jgraph.JGraph;
 import org.jgraph.graph.DefaultEdge;
@@ -18,79 +21,83 @@ import org.jgrapht.ListenableGraph;
 import org.jgrapht.ext.JGraphModelAdapter;
 import org.jgrapht.graph.ListenableDirectedGraph;
 
-public class MainAppletGKA extends JApplet {
-	private static final Color     DEFAULT_BG_COLOR = Color.decode( "#FAFBFF" );
+
+public class MainGKA extends JApplet {
+	/*private static final Color     DEFAULT_BG_COLOR = Color.decode( "#FAFBFF" );
     private static final Dimension DEFAULT_SIZE = new Dimension( 530, 320 );
-
-	private JGraphModelAdapter m_jgAdapter;
 	
-	public void init(){
-		
-		TextField logWindow = new TextField();
-		//add(logWindow);
-		add(logWindow, -1);
-		logWindow.setLocation(getWidth()+50, getHeight()+50);
-		logWindow.setText("blahhh");
-		
-		ListenableGraph g = new ListenableDirectedGraph(DefaultEdge.class);
-		
-		m_jgAdapter=new JGraphModelAdapter(g);
-		
-		JGraph jgraph = new JGraph(m_jgAdapter);
-		
-		adjustDisplaySettings( jgraph );
-        getContentPane(  ).add( jgraph );
-        resize( DEFAULT_SIZE );
+	private static JGraph jgraph;
+	private static JGraphModelAdapter m_adapter;
+	*/
 
-        // add some sample data (graph manipulated via JGraphT)
-        g.addVertex( "v1" );
-        g.addVertex( "v2" );
-        g.addVertex( "v3" );
-        g.addVertex( "v4" );
-
-        g.addEdge( "v1", "v2" );
-        g.addEdge( "v2", "v3" );
-        g.addEdge( "v3", "v1" );
-        g.addEdge( "v4", "v3" );
-
-        // position vertices nicely within JGraph component
-        positionVertexAt( "v1", 130, 40 );
-        positionVertexAt( "v2", 60, 200 );
-        positionVertexAt( "v3", 310, 230 );
-        positionVertexAt( "v4", 380, 70 );
+	public static void main(String[] args) {
 		
+		//blah
+		JFrame frame = new JFrame("GKAv2");
+		frame.setSize(600, 600);
+		
+		//JScrollPane scroller = new JScrollPane(jgraph);
+		JSplitPane splitPaneMain = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+		MainAppletGKA applet = new MainAppletGKA();
+		applet.start();
+		applet.init();
+		
+		splitPaneMain.setLeftComponent(applet.getContentPane());
+		//JSplitPane splitPaneZwei
+		JTextArea logWindow = new JTextArea("blah");
+		splitPaneMain.setRightComponent(logWindow);
+		
+		frame.add(splitPaneMain);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		/*
+		ListenableGraph g = new ListenableDirectedGraph<>(DefaultEdge.class);
+		m_adapter = new JGraphModelAdapter<>(g);
+		jgraph = new JGraph(m_adapter);
+		
+		frame.getContentPane().add(jgraph);
+		
+		
+		//frame.pack();
+		
+			
+		g.addVertex("hallo1");
+		g.addVertex("hallo2");
+		g.addEdge("hallo1","hallo2");
+		
+		//g.addEdge("hallo2", "hallo2");
+		positionVertexAt("hallo1",100,100);
+		positionVertexAt("hallo2",200,200);
+		
+		
+		jgraph.addMouseListener(new MouseAdapter(){
+			public void mousePressed(MouseEvent e){
+				//if(e.getClickCount() == 2){
+					int x= e.getX(), y=e.getY();
+					Object cell = jgraph.getFirstCellForLocation(x, y);
+					
+					System.out.println(cell.toString());
+				//}
+			}
+		});
+		*/
+
 	}
 	
-	private void adjustDisplaySettings( JGraph jg ) {
-        jg.setPreferredSize( DEFAULT_SIZE );
-
-        Color  c        = DEFAULT_BG_COLOR;
-        String colorStr = null;
-
-        try {
-            colorStr = getParameter( "bgcolor" );
-        }
-         catch( Exception e ) {}
-
-        if( colorStr != null ) {
-            c = Color.decode( colorStr );
-        }
-
-        jg.setBackground( c );
-    }
-
-
-    private void positionVertexAt( Object vertex, int x, int y ) {
-        DefaultGraphCell cell = m_jgAdapter.getVertexCell( vertex );
+	
+	/*
+	private static void positionVertexAt( Object vertex, int x, int y ) {
+        DefaultGraphCell cell = m_adapter.getVertexCell( vertex );
+        System.out.println(m_adapter.getVertexCell(vertex).toString());
+        
         Map              attr = cell.getAttributes(  );
         Rectangle2D        b    = GraphConstants.getBounds( attr );
-
         GraphConstants.setBounds( attr, new Rectangle( x, y, (int)b.getWidth(),(int) b.getHeight() ) );
-
         Map cellAttr = new HashMap(  );
         cellAttr.put( cell, attr );
-        m_jgAdapter.edit( cellAttr, null, null, null );
+        m_adapter.edit( cellAttr, null, null, null );
     }
-	
-	
+    */
+
 }
